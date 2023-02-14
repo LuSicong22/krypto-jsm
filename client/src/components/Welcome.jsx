8394220580bb45ae72b03ccbb2800a7769e729c1
simon <simon.lu@tron.network>
2023-02-14 10:41:33 +0800

    c

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,24 +5,21 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenAddress";
-// import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({ placeHolder, name, type, value, handleChange }) => {
+const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
-    placeholder={placeHolder}
+    placeholder={placeholder}
     type={type}
     step="0.0001"
     value={value}
-    onChange={(e) => {
-      handleChange(e, name);
-    }}
+    onChange={(e) => handleChange(e, name)}
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-  />;
-};
+  />
+);
 
 const Welcome = () => {
   const { currentAccount, connectWallet, handleChange, isLoading } =
@@ -73,7 +70,7 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -91,10 +88,9 @@ const Welcome = () => {
               </div>
             </div>
           </div>
-
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input
-              placeHolder="Address To"
+              placeholder="Address To"
               name="addressTo"
               type="text"
               handleChange={handleChange}
