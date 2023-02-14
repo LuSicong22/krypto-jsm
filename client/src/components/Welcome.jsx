16ded719d90892986aef5119bb38ac19f633a7f5
simon <simon.lu@tron.network>
2023-02-14 09:44:20 +0800

    c

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -15,39 +15,43 @@ const Welcome = () => {
   return (
     <div>
       <div>
-        <h1 className="text-3xl sm:text-5xl text-white text-gradient py-12 px-4">
-          Send Crypto <br />
-          accross the world
-        </h1>
-        <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-          Explore the crypto world. Buy and sell cryptocurrencies easily on
-          Krypto.
-        </p>
-        {!currentAccount && (
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-          >
-            <AiFillPlayCircle className="text-white mr-2 " />
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
-        )}
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-12 px-4">
+            Send Crypto <br />
+            accross the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and sell cryptocurrencies easily on
+            Krypto.
+          </p>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <AiFillPlayCircle className="text-white mr-2 " />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
-        <div className="">
-          <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
-            Reliability
-          </div>
-          <div className={companyCommonStyles}>Security</div>
-          <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
-            Ethereum
-          </div>
-          <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
-            Web 3.0
-          </div>
-          <div className={companyCommonStyles}>Low Fees</div>
-          <div className={`rounded-br-2xl ${companyCommonStyles}`}>
-            Blockchain
+          <div className="">
+            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+              Reliability
+            </div>
+            <div className={companyCommonStyles}>Security</div>
+            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+              Ethereum
+            </div>
+            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+              Web 3.0
+            </div>
+            <div className={companyCommonStyles}>Low Fees</div>
+            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+              Blockchain
+            </div>
           </div>
         </div>
       </div>
