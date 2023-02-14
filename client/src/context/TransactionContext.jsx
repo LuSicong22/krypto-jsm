1fc0ff5882308220f80aaf57bac22fc315680ea3
simon <simon.lu@tron.network>
2023-02-14 13:49:24 +0800

    c

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,7 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../utils/constants";
 
 export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log({
+    provider,
+    signer,
+  });
+};
 
 export const TransactionsProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
