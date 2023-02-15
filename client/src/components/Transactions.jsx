6bd6eb34fc864a4bd672142528e4544432179bef
simon <simon.lu@tron.network>
2023-02-15 15:05:28 +0800

    c

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
+
 import { TransactionContext } from "../context/TransactionContext";
+
 import useFetch from "../hooks/useFetch";
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
@@ -68,8 +70,9 @@ const TransactionsCard = ({
 
 const Transactions = () => {
   const { transactions, currentAccount } = useContext(TransactionContext);
+
   return (
-    <div className="flex w-full justify-center items-center  2xl:px-20 gradient-bg-transactions">
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
@@ -80,6 +83,7 @@ const Transactions = () => {
             Connect your account to see the latest transactions
           </h3>
         )}
+
         <div className="flex flex-wrap justify-center items-center mt-10">
           {[...dummyData, ...transactions].reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
