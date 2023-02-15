31dd223748b645aff4e15970ba55ec76e0961845
simon <simon.lu@tron.network>
2023-02-15 10:10:41 +0800

    c

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import { contractABI, contractAddress } from "../utils/constants";
+// import { contractABI, contractAddress } from "../utils/constants";
 
 export const TransactionContext = React.createContext();
 
@@ -8,17 +8,16 @@ const { ethereum } = window;
 
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
-  const signer = provider.getSigner();
-  const transactionContract = new ethers.Contract(
-    contractAddress,
-    contractABI,
-    signer
-  );
+  //   const transactionContract = new ethers.Contract(
+  //     contractAddress,
+  //     contractABI,
+  //     signer
+  //   );
 
   console.log({
     provider,
-    signer,
-    transactionContract,
+    // signer,
+    // transactionContract,
   });
 };
 
@@ -70,14 +69,17 @@ export const TransactionsProvider = ({ children }) => {
   };
 
   const sendTransaction = async () => {
-    try {
-      if (!ethereum) return alert("Please install MetaMask!");
-      const { addressTo, amount, keyword, message } = formData;
-      getEthereumContract();
-    } catch (error) {
-      console.log(error);
-      throw new Error("No ethereum object.");
-    }
+    if (!ethereum) return alert("Please install MetaMask!");
+    const { addressTo, amount, keyword, message } = formData;
+    getEthereumContract();
+    // try {
+    //   if (!ethereum) return alert("Please install MetaMask!");
+    //   const { addressTo, amount, keyword, message } = formData;
+    //   getEthereumContract();
+    // } catch (error) {
+    //   console.log(error);
+    //   throw new Error("No ethereum object.");
+    // }
   };
 
   return (
